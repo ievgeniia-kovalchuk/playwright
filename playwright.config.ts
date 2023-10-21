3386c7102bf06ff070767341c0d5959254bb5e34
ievgeniia-kovalchuk <62695010+ievgeniia-kovalchuk@users.noreply.github.com>
2023-10-21 10:27:13 +0200

    reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+<<<<<<< Updated upstream
   reporter: 'html',
+=======
+  reporter: process.env.CI ? [
+    ['dot'],
+    ['json', {  outputFile: 'test-results.json' }]
+  ] : 'list',
+>>>>>>> Stashed changes
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
