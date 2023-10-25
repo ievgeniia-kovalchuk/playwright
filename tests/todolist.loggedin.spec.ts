9472333cc14879ce3e2118d18f33895967566de5
ievgeniia-kovalchuk <ievgeniia.kovalchuk@gmail.com>
2023-10-25 12:37:02 +0200

    use hooks

--- tests/todolist.loggedin.spec.ts
+++ tests/todolist.loggedin.spec.ts
@@ -1,31 +1,33 @@
 import { test, expect } from '@playwright/test';
 import { AdminTodolistPage } from '../pages/admin.todolist.page';
 
-test('admin todolist - navigate', async ({page}) => {
-  await page.goto('simpletodolist/adminlogin.html');
-  await expect(page.locator('#eviltester')).toBeVisible();
+let adminTodolistPage;
 
-  await page.locator('#eviltester').click();
-  await expect(page.locator('.new-todo')).toBeVisible();
+test.beforeEach(async ({page}) => {
+  adminTodolistPage = new AdminTodolistPage(page);
+  adminTodolistPage.goto();
 });
 
-test('admin todolist - add items', async ({ page }) => {
-  const adminTodolistPage = new AdminTodolistPage(page);
+test.describe('todo tests - no initial data', () => {
 
-  await adminTodolistPage.goto();
-  await adminTodolistPage.addNewTodo('test item 1');
-  await expect(adminTodolistPage.todoListText).toHaveText(['test item 1']);
-  await expect(adminTodolistPage.todoCount).toHaveText('1');
-  await adminTodolistPage.addNewTodo('test item 2');
-  await expect(adminTodolistPage.todoListText).toHaveText(['test item 1', 'test item 2']);
-  await expect(adminTodolistPage.todoCount).toHaveText('2');
+  test('admin todolist - add items', async () => {
+    await adminTodolistPage.addNewTodo('test item 1');
+    await expect(adminTodolistPage.todoListText).toHaveText(['test item 1']);
+    await expect(adminTodolistPage.todoCount).toHaveText('1');
+    await adminTodolistPage.addNewTodo('test item 2');
+    await expect(adminTodolistPage.todoListText).toHaveText(['test item 1', 'test item 2']);
+    await expect(adminTodolistPage.todoCount).toHaveText('2');
+  });
 });
 
-test('admin todolist - mark all as compelete', async ({page}) => {
-  const adminTodolistPage = new AdminTodolistPage(page);
+test.describe('todo tests - fill in data', () =>
+{
+  test.beforeEach(async () => {
+    await adminTodolistPage.fillTodolist(['item1', 'item2', 'item3']);
+  });
 
-  await adminTodolistPage.goto();
-  await adminTodolistPage.fillTodolist(['item1', 'item2', 'item3']);
-  await adminTodolistPage.markAllAsComplete();
-  await expect(adminTodolistPage.todoCount).toHaveText('0');
+  test('admin todolist - mark all as compelete', async ({page}) => {
+    await adminTodolistPage.markAllAsComplete();
+    await expect(adminTodolistPage.todoCount).toHaveText('0');
+  });
 });
